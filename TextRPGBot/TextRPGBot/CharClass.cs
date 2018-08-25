daad29999b62913a2fe1e3fef33a7cbdf2c3b5d6
David Carvalho <1160640@isep.ipp.pt>
2018-08-25 16:58:48 +0100

    added CharClass

---
+++ TextRPGBot/TextRPGBot/CharClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextRPGBot
+{
+    class CharClass
+    {
+
+        private enum charClassType
+        {
+            Warrior,
+            Mage,
+            Paladin,
+            Thief,
+            Bard
+        }
+
+        private enum Personality
+        {
+
+        }
+
+        private UInt32 charClassBaseStr { get; set; }
+        private UInt32 charClassBaseInt { get; set; }
+        private UInt32 charClassBaseDxt { get; set; }
+        private UInt32 charClassBaseHlt { get; set; }
+
+
+
+        public CharClass()
+        {
+
+        }
+    }
+}
